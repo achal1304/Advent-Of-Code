f7a08275f4594b46fe71a3175329123dec8db929
achal1304 <achal20000@gmail.com>
2024-12-02 14:57:38 +0530

    day1 skeleton for input

---
+++ day1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/achal1304/Advent-Of-Code/helper"
+)
+
+// Historian Hysteria
+func main() {
+
+	leftPath := []int{}
+	rightPath := []int{}
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		parts := strings.Fields(line)
+
+		num1, err := strconv.Atoi(parts[0])
+		if err != nil {
+			log.Fatal("unable to parse parts ", err)
+		}
+		num2, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatal("unable to parse parts ", err)
+		}
+
+		leftPath = append(leftPath, num1)
+		rightPath = append(rightPath, num2)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("left", leftPath)
+	fmt.Println("right", rightPath)
+}
+
+func calculateDistance(leftPath, righPath []int) int {
+	sort.Ints(leftPath)
+	sort.Ints(righPath)
+	res := 0
+
+	for i, _ := range leftPath {
+		res += helper.AbsInt(leftPath[i] - righPath[i])
+	}
+	return res
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/achal1304/Advent-Of-Code
+
+go 1.20
